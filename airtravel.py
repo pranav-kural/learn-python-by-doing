964b472c745b9fbd49a391aec64855b1daaa2216
pranav-kural <pranavkural042@gmail.com>
2020-05-20 18:22:54 -0400

    Added the aircraft class

--- airtravel.py
+++ airtravel.py
@@ -1,8 +1,8 @@
 """ Model for aircraft flights """
 
 class Flight:
-    
-    def __init__(self, number):
+    """ A flight with a particular passenger aircraft """
+    def __init__(self, number, aircraft):
         if not number[:2].isalpha():
             raise ValueError(f"No airline code in '{number}")
         if not number[:2].isupper():
@@ -11,6 +11,30 @@ class Flight:
             raise ValueError(f"Invalid route number '{number}")
         
         self._number = number
+        self._aircraft = aircraft
+        
+    def aircraft_model(self):
+        return self._aircraft.model()
     
     def number(self):
-        return
+        return self._number
+    
+class Aircraft:
+
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+        
+    def registration(self):
+        return self._registration
+    
+    def model(self):
+        return self._model
+    
+    def seating_plan(self):
+        return (range(1, self._num_rows +1),
+                "ABCDEFGHJK"[:self._num_seats_per_row])
+    
+    
